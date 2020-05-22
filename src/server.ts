6c3a70137bf071c62f3089aea6954d5854644e94
Chris <63784760+chk-code@users.noreply.github.com>
2020-05-22 17:59:03 +0200

    add: check for environment vars

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,27 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
+  const REQUIRED_ENVIRONMENT_SETTINGS = [
+    {name:"POSTGRES_USERNAME" , message:"Usernam of Postgres DB"},
+    {name:"POSTGRES_PASSWORD" , message:"Password of Postgres DB"},
+    {name:"POSTGRES_DB" , message:"DB Name of Postgres DB"},
+    {name:"POSTGRES_HOST" , message:"Host of Postgres DB"},
+    {name:"AWS_REGION" , message:"Our AWS Region"},
+    {name:"AWS_PROFILE" , message:"The used AWS Profile"},
+    {name:"AWS_BUCKET" , message:"The S3 Bucket"},
+    {name:"URL" , message:"Local URL"},
+    {name:"JWT_SECRET" , message:"Secret for JWT"},
+    ]
+ 
+  for(var env of REQUIRED_ENVIRONMENT_SETTINGS) {
+    if (!process.env[env.name]) {
+      console.error(`Environment variable ${env.name} should be set: ${env.message}`);  
+    } else {
+      // convenient for debug; however: this line exposes all environment variable values - including any secret values they may contain
+      console.log(`Environment variable ${env.name} is set to : ${process.env[env.name]}`);  
+    }
+  }
+
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
